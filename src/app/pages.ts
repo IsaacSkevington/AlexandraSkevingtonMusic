acc138171e72f3e3d550484497492ebafacfebc7
Richard Skevington <richard.skevington@blueyonder.co.uk>
2019-11-12 21:05:21 +0000

    21/11/2019

--- src/app/pages.ts
+++ src/app/pages.ts
@@ -2,9 +2,10 @@ import { Page } from './page';
 
 export const PAGES: Page[] = [
   {name: 'Home Page', location: './home-page'},
+  {name: 'Writing', location: './writing'},
   {name: 'Instrumental Lessons', location: './instrumental-lessons'},
   {name: 'Piano Accompaniment', location: './piano-accompaniment'},
-  {name: 'Writing', location: './writing'},
+  
   {name: "Jazz Band Outreach", location:"./band"}
 ];
 
